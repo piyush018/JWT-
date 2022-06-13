079710e4a97d82235225bfbd5832f6cee26290f2
piyush <piyush@piyushs-MacBook-Air.local>
2022-06-14 00:56:27 +0530

    f

--- error/custom-error.js
+++ error/custom-error.js
@@ -1,7 +1,6 @@
 class customApiError extends Error {
-    constructor(message, statusCode) {
+    constructor(message) {
         super(message)
-        this.statusCode = statusCode
     }
 }
 module.exports = customApiError
